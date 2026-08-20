9145c4109ff1a4706abb4ecb942dca4f1726ded2
nipun-imesh <imeshnipun@gmail.com>
2026-08-20 20:12:18 +0530

    Add root endpoint for API Gateway Health Check

--- api-gateway/src/main/java/com/studentmgmt/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/studentmgmt/apigateway/ApiGatewayApplication.java
@@ -3,11 +3,19 @@ package com.studentmgmt.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "OK";
     }
 }
