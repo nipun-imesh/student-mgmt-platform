94f3e52cfcace499df7c5312152a97600ddefe05
nipun-imesh <imeshnipun@gmail.com>
2026-08-20 19:57:04 +0530

    Add root endpoint for GCP Health Check

--- config-server/src/main/java/com/studentmgmt/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/studentmgmt/configserver/ConfigServerApplication.java
@@ -3,11 +3,19 @@ package com.studentmgmt.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "OK";
     }
 }
